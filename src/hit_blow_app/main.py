4d5993aa763ec69494aecbc5aaa6ccf13bf2ab6f
kamiazya <yuki@kamiazya.tech>
2020-04-16 23:43:33 +0900

    simple question crad

--- src/hit_blow_app/main.py
+++ src/hit_blow_app/main.py
@@ -1,6 +1,44 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Optional, Tuple
+from uuid import uuid4, UUID
+
 
 app = FastAPI()
+
+
+class User(BaseModel):
+    name: str
+
+
+class Question(BaseModel):
+    id: Optional[UUID]
+    title: str
+    question: Tuple[int, int, int, int]
+
+
+class QuestionRegistry(BaseModel):
+    list: List[Question] = []
+
+    def all(self) -> Tuple[Question, ...]:
+        return tuple(self.list)
+
+    def get_by_id(self, id: UUID) -> Optional[Question]:
+        q = filter(lambda q: q.id == id, self.list)
+        return next(q, None)
+
+    def register(self, question: Question):
+        q = Question(id=uuid4(), question=question.question, title=question.title)
+        self.list.append(q)
+        return q.id
+
+    def delete_by_id(self, id: UUID) -> None:
+        q = self.get_by_id(id)
+        if q is not None:
+            self.list.remove(q)
+
+
+registry = QuestionRegistry()
 
 
 @app.get("/")
@@ -8,6 +46,17 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/questions")
+def register_question(question: Question):
+    id = registry.register(question)
+    return {"id": id}
+
+
+@app.delete("/questions/{question_id}")
+def delete_question_by_id(question_id: UUID):
+    registry.delete_by_id(question_id)
+
+
+@app.get("/questions")
+def get_questions():
+    return registry.all()
